204888fb6df9bb0e9e542ae9072f3caa66b5c405
Richard Imaoka <richard.e.imaoka@gmail.com>
2018-08-21 20:41:16 +0900

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,10 @@
-import scala.concurrent.duration._
 import akka.actor._
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.HttpResponse
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
 
 object Main extends App with RestInterface {
 
@@ -15,7 +16,10 @@ object Main extends App with RestInterface {
   implicit val executionContext = system.dispatcher
   implicit val timeout = Timeout(10 seconds)
 
-  val api = routes
+  val interfaces = new RestInterface {
+    override implicit def executionContext: ExecutionContext = system.dispatcher
+  }
+  val api = interfaces.routes
 
   Http().bindAndHandle(handler = api, "localhost", 8080) map {
     binding => println(s"REST interface bound to ${binding.localAddress}")
